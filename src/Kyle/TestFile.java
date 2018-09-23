2e5aaad3557df80863bf2b227953e930811e36b7
Kyle <38015393+ProDevKyle@users.noreply.github.com>
2018-09-23 15:04:58 -0400

    Update TestFile.java

--- src/Kyle/TestFile.java
+++ src/Kyle/TestFile.java
@@ -1,8 +1,15 @@
 package Kyle;
 
-public class TestFile {
+public class TestFile
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println(CTamLib.isPalindrome("racecar"));
+        System.out.println(CTamLib.dateStr("10/06/2003"));
+        System.out.println(CTamLib.cutOut("TodayIsNotWarm","Not"));
+        System.out.println(CTamLib.isFibonnaci(8));
+        System.out.println(CTamLib.sumUpTo(5));
         System.out.println(CTamLib.leastCommonMultiple(1,5,6));
     }
 }
